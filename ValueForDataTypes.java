091b59aa92fae455a78e37d3715a5783dc316f54
laxminarayana0295 <149564244+laxminarayana0295@users.noreply.github.com>
2024-08-25 15:26:19 +0530

    Add files via upload

---
+++ ValueForDataTypes.java
@@ -0,0 +1,38 @@
+
+
+public class ValueForDataTypes {
+
+	public static void main(String[] args) {
+		
+		
+		boolean bb= true; //we can only asign either true or false
+		
+		
+		byte by= -128; //we can store or asign value upto -128 to 127
+		
+		
+		float fl=(float)9.9;//  we can asign decimal  values  to  float
+		
+		
+		char ch='c';  // we can asign single alphbet to char by using single  codes ''
+		
+		
+		int num = 999999999; // we can only store or asign  only numeric values both
+		                      //negative and positive but not decimal upto 999999999
+		
+		
+		short sh= 10000;// we can only store or asign  only numeric values both
+          					//negative and positive but not decimal -10000 to 10000
+		
+		
+		long l= 8999999999999999999l; // we can only store or asign  only numeric values both
+                                      //negative and positive but not decimal 8999999999999999999l
+		 							  // by using l as aextension that  make more space
+		
+		
+		double  dd=  123.4;// we mostly use double  to aign decimal  values
+		
+		
+	}
+
+}
